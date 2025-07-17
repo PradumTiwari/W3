a480ef5d9dbce907e5f11bd73cd45e60cdebde07
Pradum <satendarpradum@gmail.com>
2025-07-17 14:12:47 +0530

    added struct

--- Solana/FirstSolanaContract/src/lib.rs
+++ Solana/FirstSolanaContract/src/lib.rs
@@ -4,10 +4,19 @@ use solana_program::{
     entrypoint::ProgramResult,
     pubkey::Pubkey,
     msg,
-    program_error::ProgramError,
+    program_error::ProgramError
 };
 
 entrypoint!(process_instruction);
+
+use borsh::{BorshDeserialize,BorshSerialize};
+
+#[derive(BorshSerialize,BorshDeserialize,Debug)]
+
+pub struct Counter{
+    pub count:u8,
+}
+
 
 pub enum CounterInstruction{
     Increment,
@@ -17,12 +26,10 @@ pub enum CounterInstruction{
 
 impl CounterInstruction{
     pub fn unpack(input:&[u8])->Result<Self,ProgramError>{
-        let (&tag,_)=input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
-
-        match tag{
-            0=>Ok(Self::Increment),
-            1=>Ok(Self::Decrement),
-            2=>Ok(Self::Reset),
+        match input{
+            [0]=>Ok(Self::Increment),
+            [1]=>Ok(Self::Decrement),
+            [2]=>Ok(Self::Reset),
             _=>Err(ProgramError::InvalidInstructionData),
         }
     }
@@ -33,26 +40,34 @@ pub fn process_instruction(
     accounts:&[AccountInfo],
     instruction_data:&[u8],
 )->ProgramResult{
-    let account=next_account_info(&mut accounts.iter())?;
-    let data=&mut *account.try_borrow_mut_data()?;
-    let counter = &mut data[0];
 
+    let account=next_account_info(&mut accounts.iter())?;
+    let data=account.try_borrow_mut_data()?;
+
+    //Deserialize the counter from the byte array
+    let mut counter=Counter::try_from_slice(&data)?;
+
+    //Decode the instruction
     let instruction=CounterInstruction::unpack(instruction_data)?;
 
     match instruction{
-        CounterInstruction::Increment=>{
-            *counter=counter.wrapping_add(1);
-        }
-
         CounterInstruction::Decrement=>{
-            *counter=counter.wrapping_sub(1);
-        }
+            msg!("Decrementing");
+            counter.count=counter.count.wrapping_sub(1);
 
+        }
+        CounterInstruction::Increment=>{
+            msg!("Incrementing");
+            counter.count=counter.count.wrapping_add(1);
+        }
         CounterInstruction::Reset=>{
-            *counter=0;
+            msg!("Reseting");
+            counter.count=0;
         }
     }
+  
+  //serialize the counter back to bytes
+  counter.serialize(&mut *data)?;
 
-
-    Ok(())
+     Ok(())
 }
